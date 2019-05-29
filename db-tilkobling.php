36ad05744722a247fe8ec6766542935683d56e2d
Simon Torstensen <torstensen.simon@gmail.com>
2019-05-29 14:44:16 +0200

    Update db-tilkobling.php

--- db-tilkobling.php
+++ db-tilkobling.php
@@ -1,8 +1,8 @@
 <?php
   $host="localhost";
-  $user="146813";
-  $password="Dina002";
-  $database="146813";
+  $user="";
+  $password="";
+  $database="";
   $db=mysqli_connect($host, $user, $password, $database) or die("Ikke kontakt med database-server");
 
 
